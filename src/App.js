234ede28949a24c93146ed438ba5cdca5ab4bb47
khanjasir90 <khanmohdjasir@gmail.com>
2022-09-27 22:05:11 +0530

    added card and openweather api integration
    Signed-off-by: khanjasir90 <khanmohdjasir@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Card from './components/Card' 
+import Axios from 'axios'
 import { TextField} from '@mui/material'
 
 import './App.css'
 
+
+
 const App = () => {
+
+  const [city,setCity] = useState("");
+  const [weatherData,setWeatherData] = useState("");
+  async function inputHandler() {
+    if(city === "") alert('Enter a Vaid City Name...')
+    const response = await Axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2945363fc971f134d5aa09136cb398dd
+      `,
+    );
+    setWeatherData(response.data);
+    console.log(response.data);
+  }
+
+  function handleChange(e) {
+    setCity(e.target.value);
+  }
+
   return (
     <div className='home_container'>
       <div className='grid_container_1'>
@@ -16,12 +37,17 @@ const App = () => {
           <h1>Weather Buddy</h1>
         </div>
         <div className='grid_container_2_input'>
-          <TextField id="outlined-basic" label="Enter your City" variant="outlined"/>
-          <button>Submit</button>
+          <TextField id="outlined-basic" label="Enter your City" variant="outlined" value={city} onChange={handleChange}/>
+          <button onClick={inputHandler}>Submit</button>
+        </div>
+        <div className='card_container'>
+        {weatherData != "" &&
+          <Card data={weatherData}/>
+        }
         </div>
      </div>
     </div>
-  )
+  );
 }
 
 export default App
